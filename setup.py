df71e306126471ee85e023c457f50873f082b39f
Elias Ågeby <eageby@kth.se>
2020-07-16 10:04:17 +0200

    Setuptools

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='tb-extractor', version='0.1', packages=find_packages(),
+       entry_points='''[console_scripts]
+        tb-extractor=extractor:main
+    ''',)
