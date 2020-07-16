428034eb9ffca99be95260c93c4c61c83d7ca472
Elias Ågeby <eageby@kth.se>
2020-07-16 10:21:04 +0200

    Entry point

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(name='tb-extractor', version='0.1', packages=find_packages(),
-       entry_points='''[console_scripts]
+setup(
+    name="tb-extractor",
+    version="0.1",
+    py_modules=["extractor"],
+    entry_points="""[console_scripts]
         tb-extractor=extractor:main
-    ''',
-    install_requires=[
-        'click',
-        'tensorboard',
-        'pandas',
-        'pathlib')
+    """,
+    install_requires=["click", "tensorboard", "pandas", "pathlib"],
+)
